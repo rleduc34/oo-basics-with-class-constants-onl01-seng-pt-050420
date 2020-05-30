c7fa313cc7116b2c3227fba0723cc09e73d0f18c
Robert Leduc <rleduc34@gmail.com>
2020-05-30 00:34:31 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,6 +6,8 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+    if !(BRANDS.include? (@brand))
+      BRANDS << @brand
   end
 
   def cobble
@@ -13,8 +15,4 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-  def brand=(brand)
-    @brand = brand
-    BRANDS << brand
-  end
 end
